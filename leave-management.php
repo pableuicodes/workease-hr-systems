2039c9d866e351f654ac5c5a13a08fe4352b224e
Paul Justine Posas <196165811+pableuicodes@users.noreply.github.com>
2026-03-20 05:13:25 +0800

    Add files via upload

---
+++ leave-management.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>WorkEase | Leave Management</title>
+    <link rel="stylesheet" href="./css/style2.css" />
+    <script type="text/javascript" src="app.js" defer></script>
+  </head>
+  <body>
+
+  <?php
+      $pageTitle = 'leave-management';
+      // $activePage = '';
+  ?>
+    <!-- separate file for navigation -->
+<?php include './inc/nav.inc.php' ?>
+    <main>
+      <div class="page-title-container">
+       Leave Management
+      </div>
+
+    </main>
+  </body>
+</html>
